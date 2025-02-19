a09ee9fd04f165fba2ff8db22aa8f2f5d6394c25
iamGiaSi <giasinguyentran@gmail.com>
2025-02-19 20:16:55 +0700

    Update: README.md

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Nguyễn Trần Gia Sĩ
 # Bài 1
+![Bài 01](./BaiTapTuan2-ReactJS/HinhAnhMinhChung/Bai01.PNG)
 
 # Bài 2
+![Bài 02](./BaiTapTuan2-ReactJS/HinhAnhMinhChung/Bai02.PNG)
 
 # Bài 3
-
+![Bài 03](./BaiTapTuan2-ReactJS/HinhAnhMinhChung/Bai03.PNG)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import Bai01 from './components/NguyenTranGiaSi_Bai01';
 import Bai02 from './components/NguyenTranGiaSi_Bai02';
@@ -8,6 +7,10 @@ function App() {
   return (
     <>
       <Bai01/>
+      <br/>
+      <Bai02/>
+      <br/>
+      <Bai03/>
     </>
   );
 }
